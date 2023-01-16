666c051f6dda39555d10693a7d6fe594b03d5cac
Cassandra Belyeu <cassiebelyeu96@gmail.com>
2023-01-15 21:05:31 -0600

    Completed Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentageString = Console.ReadLine();
+        int percentInt = int.Parse(percentageString);
+        string letter = "F";
+        if (percentInt >= 90) {
+            letter = "A";
+        }
+        else if (percentInt >= 80) {
+            letter = "B";
+
+        }
+        else if (percentInt >= 70) {
+            letter = "C";
+        }
+        else if (percentInt >= 60) {
+            letter = "D";
+        }
+
+        string plusOrMinus = "";
+        if (letter != "F")
+        {
+            int lastDigit = percentInt % 10;
+            if (lastDigit >= 7 && percentInt < 97)
+            {
+                plusOrMinus = "+";
+            }
+            else if (lastDigit < 3 && percentInt <= 93)
+            {
+                plusOrMinus = "-";
+            }
+        }
+        
+        Console.WriteLine($"Your grade is: {letter}{plusOrMinus}");
+        if (percentInt >= 70) {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else {
+            Console.WriteLine("You didn't pass the class, but there's always retaking!");
+        }
     }
 }
