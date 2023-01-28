9c33b1e996cb95890c8bf64d4d5b5616f6e4dfbc
cbelyeu <98199426+cbelyeu@users.noreply.github.com>
2023-01-28 15:45:47 -0600

    Update Program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,16 +4,22 @@ class Program
 {
     static void Main(string[] args) {
         Job job1 = new Job();
-        job1._company = "evil.inc";
+        job1._company = "Evil.inc";
         job1._jobTitle = "henchman";
         job1._startYear = 1800;
         job1._endYear = 1900;
 
         Job job2 = new Job();
-        job2._company = "heros.co";
+        job2._company = "Heros.co";
         job2._jobTitle = "sidekick";
         job2._startYear = 1911;
-        job2._endYear = 2020;
+        job2._endYear = 2019;
+        
+        Job job3 = new Job();
+        job3._company = "Plague.inc";
+        job3._jobTitle = "Successful Variant";
+        job3._startYear = 2020;
+        job3._endYear = 2022;
 
         Resume resume1 = new Resume();
         resume1._name = "Minion #23";
