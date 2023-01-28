c52885aff588a950c38c910d6e81640defa0feac
Cassandra Belyeu <cassiebelyeu96@gmail.com>
2023-01-28 15:34:22 -0600

    Practiced c# resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,25 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning02 World!");
+    static void Main(string[] args) {
+        Job job1 = new Job();
+        job1._company = "evil.inc";
+        job1._jobTitle = "henchman";
+        job1._startYear = 1800;
+        job1._endYear = 1900;
+
+        Job job2 = new Job();
+        job2._company = "heros.co";
+        job2._jobTitle = "sidekick";
+        job2._startYear = 1911;
+        job2._endYear = 2020;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Minion #23";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        
+        resume1.Display();
     }
 }
+
