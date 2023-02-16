d253e22da8b7a567e6b44a6dc54aae0229af4548
Cassandra Belyeu <cassiebelyeu96@gmail.com>
2023-02-15 18:00:14 -0600

    Encapsulation

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction emptyFraction = new Fraction();
+        Console.Write($"{emptyFraction.GetFractionString()}\n");
+        Console.Write($"{emptyFraction.GetDecimalValue()}\n");
+        Fraction oneFraction = new Fraction(6);
+        Console.Write($"{oneFraction.GetFractionString()}\n");
+        Console.Write($"{oneFraction.GetDecimalValue()}\n");
+        Fraction twoFraction = new Fraction(6,7);
+        Console.Write($"{twoFraction.GetFractionString()}\n");
+        Console.Write($"{twoFraction.GetDecimalValue()}\n");
+        Fraction twoFraction2 = new Fraction(7,12);
+        Console.Write($"{twoFraction2.GetFractionString()}\n");
+        Console.Write($"{twoFraction2.GetDecimalValue()}\n");
     }
 }
