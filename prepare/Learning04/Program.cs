999d22ce8be1bed3ab0fac4e32ec08e2530d53de
Cassandra Belyeu <cassiebelyeu96@gmail.com>
2023-02-27 22:28:08 -0600

    Inheritance

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,10 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Assignment
         Assignment simpleAssignment = new Assignment("Joquavius the Third", "World Domination");
         Console.WriteLine("\n" + simpleAssignment.GetSummary());
+        //MathAssignment
         MathAssignment M = new MathAssignment("Pickles", "Regret", "78970","2.3-4.8");
         Console.WriteLine("\n" + M.GetSummary());
+        //WritingAssignment
         WritingAssignment W = new WritingAssignment("Calvin","Creative Writing","The Continued Adventures of Spaceman Spiff");
         Console.WriteLine("\n" + W.GetSummary() + "\n");
     }
